425955d2c2e658a569b156ff57ca136268a1f3c0
souravrane <sourav1121997@gmail.com>
2021-12-12 13:55:29 +0530

    footer created

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
 export const Footer = () => {
+  const year = new Date().getFullYear();
   return (
-    <div>
-      <h1>Footer</h1>
+    <div className="text-center p-10 mt-10 border-t dark:border-gray-700 border-gray-200">
+      <h1>@{year} Google Clone, Inc.</h1>
     </div>
   );
 };
